d1d730d4dba7168167e968e18c4d6a8209d9aba7
Mert <mert.agcakoyun@stu.fsm.edu.tr>
2020-08-05 16:52:58 +0300

    MVC Integration

--- ToDo/ToDo.Web/Startup.cs
+++ ToDo/ToDo.Web/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ToDo.Web.Middlewares;
 
 namespace ToDo.Web
 {
@@ -16,6 +17,7 @@ namespace ToDo.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,13 +29,14 @@ namespace ToDo.Web
             }
 
             app.UseRouting();
+            
+            app.UseCustomStaticFiles();
 
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints(endpoints => 
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name:"default",
+                    pattern:"{controller=Home}/{action=Index}");
             });
         }
     }
