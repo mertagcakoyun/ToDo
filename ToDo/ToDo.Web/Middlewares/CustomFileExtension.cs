c97d9e682a6ed6c1a7e0c2060073ae92d55e8d7c
Mert <mert.agcakoyun@stu.fsm.edu.tr>
2020-08-10 14:04:19 +0300

    MVC ıntegration v2

---
+++ ToDo/ToDo.Web/Middlewares/CustomFileExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace ToDo.Web.Middlewares
+{
+    public static class CustomFileExtension
+    {
+        public static void UseCustomStaticFiles(this IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+        }
+    }
+}
---
+++ ToDo/ToDo.Web/Models/UserSignUpViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo.Web.Models
+{
+    public class UserSignUpViewModel
+    {
+        
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
